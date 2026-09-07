f862f9d69ae7e70908952964a3d660d9d25115b7
Regina <reginagalvezch@gmail.com>
2026-09-07 12:57:49 -0300

    add type file

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type JobState =
+  | "queued"
+  | "running"
+  | "succeeded"
+  | "failed";
+
+export type JobHandler<T> = () => Promise<T>;
+
+export interface JobOptions {
+  maxRetries?: number;
+}
+
+export interface JobSnapshot<T = unknown> {
+  id: string;
+  state: JobState;
+  attempts: number;
+  result?: T;
+  error?: unknown;
+}
